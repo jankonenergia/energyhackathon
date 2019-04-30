0501cd9e62180cac678dd41855661bc99c463845
Samuli Polvi <samuli.polvi@magicad.com>
2019-05-01 00:54:40 +0300

    Init React with Apollo Client 🚀

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+## Jankon Energia Client
+
+__yarn start__
+*Starts the development server.*
+
+**yarn build**
+*Bundles the app into static files for production.*
+
+**yarn test**
+*Starts the test runner.*
+
+
+![asiakunnossa](https://thumbs.gfycat.com/NaughtyNearIvorygull-size_restricted.gif)
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import asiakunnossa from './images/asiakunnossa.gif';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={asiakunnossa} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
