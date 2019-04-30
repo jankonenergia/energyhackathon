837e3785f8916d93e33e5a59c3c206863ea0883b
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-04-30 23:49:41 +0300

    Added a authorize handler for jwt

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 import { ApolloServer, gql } from 'apollo-server'
+import jwt from 'jsonwebtoken'
+import util from 'util'
+
 const { typeDefs } = require('./schema')
 const { resolvers } = require('./resolvers')
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const bearerToken = req.headers.authorization ? req.headers.authorization.replace('Bearer ', '') : null
+    if (bearerToken) {
+        const payload = await util.promisify(jwt.verify)(bearerToken, 'jankonenergia') //TODO: Move to env variable
+        return { user: payload.user }
+    }
+    return null
+  } 
 })
 
 server.listen().then(({ url }) => {
