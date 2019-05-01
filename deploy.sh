cbbe2bcba69b1f804fb41e73780d2e8d66244843
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 14:08:19 +0300

    ahh yes, the famous typo

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
 ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
 ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/backend:latest jankonenergia_backend'
-ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE"--image jankonenergia/frontend:latest jankonenergia_frontend'
+ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/frontend:latest jankonenergia_frontend'
