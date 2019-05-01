e3cf78d3eb34fdb52e660ac9e5a8c851c925ea3a
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 15:14:14 +0300

    test 2

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ echo "***********[ Pulling latest images ]***********"
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
 echo "***********[ Updating images]***********"
-ssh $SERVER_USERNAME@jankon.energy "sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/backend:latest jankonenergia_backend"
-ssh $SERVER_USERNAME@jankon.energy "sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/frontend:latest jankonenergia_frontend"
+BACKEND="sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/backend:latest jankonenergia_backend"
+FRONTEND="sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/frontend:latest jankonenergia_frontend"
+echo $BACKEND
+ssh $SERVER_USERNAME@jankon.energy $FRONTEND
+ssh $SERVER_USERNAME@jankon.energy $BACKEND
