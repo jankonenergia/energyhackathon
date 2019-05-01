0a506b96a70fdeae42b5114a4d4a474830e76f4f
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 16:17:06 +0300

    removed useless echo

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:lates
 echo "***********[ Updating images]***********"
 BACKEND="sudo docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --env-add COMMIT='$TRAVIS_COMMIT' --env-add COMMIT_MESSAGE='$TRAVIS_COMMIT_MESSAGE' --image jankonenergia/backend:latest jankonenergia_backend"
 FRONTEND="sudo docker service update --env-add VERSION='$TRAVIS_BUILD_NUMBER' --env-add COMMIT='$TRAVIS_COMMIT' --env-add COMMIT_MESSAGE='$TRAVIS_COMMIT_MESSAGE' --image jankonenergia/frontend:latest jankonenergia_frontend"
-echo $BACKEND
 ssh $SERVER_USERNAME@jankon.energy $FRONTEND
 ssh $SERVER_USERNAME@jankon.energy $BACKEND
