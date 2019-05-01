29691f14f83e87512e0dba4db44fbae3c9d77ebe
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 06:10:59 +0300

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
+ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
 ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --image jankonenergia/backend:latest jankonenergia_backend'
 ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --image jankonenergia/frontend:latest jankonenergia_frontend'
