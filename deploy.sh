5224331329a1d16d9e31872a9abc19bb0455a478
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 15:03:03 +0300

    learned something new from bash scripting. double quotes mean something.

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ echo "***********[ Pulling latest images ]***********"
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
 echo "***********[ Updating env variables ]***********"
-ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE jankonenergia_backend'
-ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE jankonenergia_frontend'
+ssh $SERVER_USERNAME@jankon.energy "sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE jankonenergia_backend"
+ssh $SERVER_USERNAME@jankon.energy "sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE jankonenergia_frontend"
 echo "***********[ Updating Images to latest ]***********"
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --image jankonenergia/backend:latest jankonenergia_backend'
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --image jankonenergia/frontend:latest jankonenergia_frontend'
