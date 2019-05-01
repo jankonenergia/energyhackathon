9e235f35b43bccc9bc54eebedb3140fd273d9f53
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 14:18:53 +0300

    removed rsa set

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
-ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
-ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/backend:latest jankonenergia_backend'
-ssh -i deploy_rsa $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/frontend:latest jankonenergia_frontend'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/backend:latest jankonenergia_backend'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/frontend:latest jankonenergia_frontend'
