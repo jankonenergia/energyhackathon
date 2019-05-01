f6d8bec7ccd69e28acb26572f971135ea9af70e1
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-05-01 14:27:18 +0300

    version bump

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "$TRAVIS_BUILD_NUMBER"
+echo "$TRAVIS_COMMIT"
+echo "$TRAVIS_COMMIT_MESSAGE"
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/frontend:latest'
 ssh $SERVER_USERNAME@jankon.energy 'sudo docker pull jankonenergia/backend:latest'
-ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/backend:latest jankonenergia_backend'
-ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add "VERSION=$TRAVIS_BUILD_NUMBER" --env-add "COMMIT=$TRAVIS_COMMIT" --env-add "COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE" --image jankonenergia/frontend:latest jankonenergia_frontend'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/backend:latest jankonenergia_backend'
+ssh $SERVER_USERNAME@jankon.energy 'sudo docker service update --env-add VERSION=$TRAVIS_BUILD_NUMBER --env-add COMMIT=$TRAVIS_COMMIT --env-add COMMIT_MESSAGE=$TRAVIS_COMMIT_MESSAGE --image jankonenergia/frontend:latest jankonenergia_frontend'
