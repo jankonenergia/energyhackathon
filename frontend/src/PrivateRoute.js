0cb97e15c553b7803d5f7ae7a3d328736a9ff8eb
Samuli Polvi <samuli.polvi@magicad.com>
2019-05-18 14:52:09 +0300

    Login as default user

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { Route, Redirect } from "react-router-dom";
 import MainWrapper from './containers/MainWrapper';
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => (
-      localStorage.getItem('token')
-        ? (
-          <MainWrapper history={props.history}>
-            <Component {...props} />
-          </MainWrapper>
-        )
-        : <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location }
-          }} />
-    )} />
-)
-
-export default PrivateRoute;
+export default class PrivateRoute extends React.Component {
+  componentWillMount() {
+    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVjZGZlOWM4MTM1NDczMDAxMzVhZmFkYyIsIm5pY2tuYW1lIjoidGt1ZW5lcmd5IiwiZmlyc3ROYW1lIjoiVHVya3UiLCJsYXN0TmFtZSI6IkVuZXJneWhhY2siLCJjcmVhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJ1cGRhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJfX3YiOjB9LCJpYXQiOjE1NTgxNzgyNTd9.S6z5_dFuVVf43wE6YacNooRnL-EP0jMVBmu4SxPo154')
+    localStorage.setItem('id', '5cdfe9c813547300135afadc')
+  }
+  render() {
+    const { component: Component, ...rest } = this.props
+    return (
+      <Route
+        {...rest}
+        render={(props) => (
+          localStorage.getItem('token')
+            ? (
+              <MainWrapper history={props.history}>
+                <Component {...props} />
+              </MainWrapper>
+            )
+            : <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: props.location }
+              }} />
+        )} />
+    )
+  }
+}
