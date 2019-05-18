3751e3e42ac1dd41c2ed0a2ebfd5ba56e240ba6d
Samuli Polvi <samuli.polvi@magicad.com>
2019-05-18 05:18:25 +0300

    Friends

---
+++ frontend/src/components/AddFriendForm.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import '../App.scss';
+
+export default class AddFriendForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickname: '',
+      errors: {
+        nickname: 'first',
+      }
+    }
+  };
+
+  handleChange = input => event => {
+    this.validate(input, event.target.value)
+    this.setState({ [input]: event.target.value });
+  }
+
+  onSubmit = (nickname, password) => event => {
+    const { onSubmit } = this.props;
+    event.preventDefault();
+    const hasErrors = Object.keys(this.state.errors).map(p => this.state.errors[p]).includes(true);
+    const hasFirst = Object.keys(this.state.errors).map(p => this.state.errors[p]).includes('first');
+    if (hasFirst) {
+      const errors = this.state.errors;
+      Object.keys(this.state.errors).forEach(e => {
+        if (this.state.errors[e] === 'first') {
+          errors[e] = true;
+        }
+      });
+      this.setState({ errors })
+    }
+    if (!hasErrors && !hasFirst) onSubmit(nickname, password);
+  }
+
+  validate = (input, value) => {
+    const { errors } = this.state;
+    if (!value) {
+      this.setState({ errors: { [input]: true } })
+    }
+
+    else {
+      this.setState({ errors: { ...errors, [input]: false } })
+    }
+  }
+
+
+  render() {
+    const { nickname, password, errors } = this.state;
+
+    return (
+      <form
+        onSubmit={this.onSubmit(nickname, password)} noValidate autoComplete="false">
+        <Grid
+          container
+          spacing={16}
+          direction="column"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid container direction="row">
+          {this.props.noFriends && <p> No friends yet :( </p>}
+            <Grid item xs={12}>
+              <TextField
+                error={errors.nickname === true}
+                id="nickname"
+                label="Username"
+                value={this.state.nickname}
+                onChange={this.handleChange('nickname')}
+                fullWidth
+                margin="normal"
+                required
+              />
+            </Grid>
+          </Grid>
+          <Button fullWidth type="submit" style={{ marginTop: '16px' }} variant="contained" color="primary">
+            Add friend 😍
+          </Button>
+        </Grid>
+      </form>
+    );
+  }
+}
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,3 +3,4 @@ export { default as RegisterForm } from './RegisterForm';
 export { default as MainDrawer } from './MainDrawer';
 export { default as HousingForm } from './HousingForm';
 export { default as BottomNav } from './BottomNav';
+export { default as AddFriendForm } from './AddFriendForm';
