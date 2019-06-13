4f7f5c86b0938e9e4eab0f4c13f0a4231a048009
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-06-13 15:43:05 +0300

    removed deploy and default user

--- frontend/src/containers/LandingPage.js
+++ frontend/src/containers/LandingPage.js
@@ -5,8 +5,6 @@ import Person from '../Person';
 import { Button } from '@material-ui/core';
 export default class LandingPage extends React.PureComponent {
   componentWillMount() {
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVjZGZlOWM4MTM1NDczMDAxMzVhZmFkYyIsIm5pY2tuYW1lIjoidGt1ZW5lcmd5IiwiZmlyc3ROYW1lIjoiVHVya3UiLCJsYXN0TmFtZSI6IkVuZXJneWhhY2siLCJjcmVhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJ1cGRhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJfX3YiOjB9LCJpYXQiOjE1NTgxNzgyNTd9.S6z5_dFuVVf43wE6YacNooRnL-EP0jMVBmu4SxPo154')
-    localStorage.setItem('id', '5cdfe9c813547300135afadc')
     if (localStorage.getItem('token')) {
       this.props.history.push('/home');
     }
