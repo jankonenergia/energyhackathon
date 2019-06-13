c0bc29a251b2217c9c15c011a11fb2117ead662a
Toni Iltanen <toni.iltanen@codecontrol.fi>
2019-06-13 16:05:55 +0300

    removed fixed token

--- frontend/src/PrivateRoute.js
+++ frontend/src/PrivateRoute.js
@@ -3,10 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 import MainWrapper from './containers/MainWrapper';
 
 export default class PrivateRoute extends React.Component {
-  componentWillMount() {
-    localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVjZGZlOWM4MTM1NDczMDAxMzVhZmFkYyIsIm5pY2tuYW1lIjoidGt1ZW5lcmd5IiwiZmlyc3ROYW1lIjoiVHVya3UiLCJsYXN0TmFtZSI6IkVuZXJneWhhY2siLCJjcmVhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJ1cGRhdGVkQXQiOiIyMDE5LTA1LTE4VDExOjE3OjI4LjQyOFoiLCJfX3YiOjB9LCJpYXQiOjE1NTgxNzgyNTd9.S6z5_dFuVVf43wE6YacNooRnL-EP0jMVBmu4SxPo154')
-    localStorage.setItem('id', '5cdfe9c813547300135afadc')
-  }
+
   render() {
     const { component: Component, ...rest } = this.props
     return (
